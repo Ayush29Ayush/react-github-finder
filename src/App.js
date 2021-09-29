8de5c661ffa4a993044f2c7ccfbae6a6f9a40c94
Ayush <ayushsenapati123@gmail.com>
2021-09-30 02:55:04 +0530

    List and passing state with props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
-import UserItem from './components/users/UserItem'
+import Users from './components/users/Users'
 import './App.css';
 
 class App extends Component {
   
   render() {
-    // const numbers = [1,2,3,4];
-
     return (
       <div className="App">
-    
         <Navbar /> 
-        <UserItem />
+
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,41 +1,19 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    // Using constructor for making state , runs automatically when the component runs
-    // constructor() {
-    //     //! we need to call super to call a parent class constructor
-    //     super()
-    //     // console.log(123)
-    //     this.state = {
-    //         id: 'id',
-    //         login: 'mojombo',
-    //         avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    //         html_url: 'https://github.com/mojombo'
-    //     }
-    // }
-
-    // It is not a necessary to give state in a constructor
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    }
-
+    
     render() {
         //! Here we are destructuring 
-        const {login,avatar_url,html_url} = this.state;
+        const {login,avatar_url,html_url} = this.props.user; // user is the prop name
 
         return (
             <div className="card text-center">
-                {/* This is UserItem */}
-                {/* we are giving width using inline CSS */}
-                {/* <img src={this.state.avatar_url} alt="" className="round-img" style={{ width: '60px' }} /> */}
+               
                 <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
-                {/* <h3>{this.state.login}</h3> */}
+                
                 <h3>{login}</h3>
+
                 <div>
-                    {/* <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">More</a> */}
                     <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
                 </div>
             </div>
