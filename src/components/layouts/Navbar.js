6a67cc4b64076b24921b0254e07ea5dc0585644d
Ayush <ayushsenapati123@gmail.com>
2021-10-03 20:49:40 +0530

    added changes in navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React from "react";
+import PropTypes from "prop-types";
 
 // const Navbar = (props) => {
-const Navbar = ({ icon,title }) => {
-
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    {/* <i className={props.icon}/> {props.title} */}
-                    <i className={icon}/> {title}
-                </h1>
-            </nav>
-        )
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        {/* <i className={props.icon}/> {props.title} */}
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
 
 // Default Props
 Navbar.defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github'
+  title: "Github Finder",
+  icon: "fab fa-github",
 };
 
 // PropTypes => Used to set the "Type" for the Props
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
-export default Navbar
+export default Navbar;
