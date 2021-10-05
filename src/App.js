8f8dd72031dfe9cdfbe62134770f4f12f4957906
Ayush <ayushsenapati123@gmail.com>
2021-10-05 21:10:44 +0530

    repos and repoitem component and data

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
+    
   };
 
   //! Search Github users
@@ -29,6 +31,18 @@ class App extends Component {
   };
 
   //! Get single Github user
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+
+    // this.setState({ users: res.data.items, loading: false });
+    this.setState({ repos: res.data, loading: false });
+  };
+
+  //! Get users repos
   getUser = async (username) => {
     this.setState({ loading: true });
 
@@ -51,7 +65,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       //! Making a default home page/directory using router
@@ -85,7 +99,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
