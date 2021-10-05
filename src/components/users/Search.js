98abecea33e9531937cb99bf0f7ae0a6f8ac8ad4
Ayush <ayushsenapati123@gmail.com>
2021-10-06 00:51:24 +0530

    refactoring to hooks and context

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,62 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export class Search extends Component {
-  state = {
-    text: "",
-  };
+// const Search = (props) => {
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+  // state = {
+  //   text: "",
+  // };
+  //! defining state
+  const [text, setText] = useState("");
 
-  static propTypes = {
-    //! type "ptfr" for proptypes func required
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter something', 'light')
+    // if (this.state.text === "") {
+    if (text === "") {
+      // this.props.setAlert("Please enter something", "light");
+      setAlert("Please enter something", "light");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      // searchUsers(this.state.text);
+      searchUsers(text);
+      // this.setState({ text: "" });
+      setText("");
     }
   };
 
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  // const onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => setText(e.target.value);
 
-  render() {
-    const { showClear, clearUsers } = this.props
+  return (
+    <div>
+      <form onSubmit={onSubmit.bind(this)} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          // value={this.state.text}
+          value={text}
+          // without onChange , we cannot give input
+          // onChange={this.onChange}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <form onSubmit={this.onSubmit.bind(this)} className="form">
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            value={this.state.text}
-            // without onChange , we cannot give input
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {showClear && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={clearUsers}
-          >
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+Search.propTypes = {
+  //! type "ptfr" for proptypes func required
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
